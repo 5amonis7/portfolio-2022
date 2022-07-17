f5621ed5ccdd8e6870bd403ae3fd449b73210b4d
Andrew Monis <5andrewmonis7@gmail.com>
2022-07-16 23:45:53 -0400

    heroku added

--- express.js
+++ express.js
@@ -2,7 +2,12 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.listen(3000, function () {
+
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000
+}
+app.listen(port, function () {
     console.log('Server is running')
 })
 
