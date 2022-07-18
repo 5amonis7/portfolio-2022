1fd739f024591955580ba6e2d3dfa23366c9b245
Andrew Monis <5andrewmonis7@gmail.com>
2022-07-18 13:12:33 -0400

    fix

--- express.js
+++ express.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 // require('dotenv').config();
 // Database = 'mongodb+srv://amonis:$Code&&Sleep7@portfolio.j61o7hs.mongodb.net/portfolio'
 
-mongoose.connect('mongodb+srv://amonis:$Code&&Sleep7@portfolio.j61o7hs.mongodb.net/portfolio', { useNewUrlParser: true })
+mongoose.connect(process.env.Database, { useNewUrlParser: true })
 
 const blogSchema = new mongoose.Schema({
   name: String,
